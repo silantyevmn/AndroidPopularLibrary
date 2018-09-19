cf49cb7169d9c6e3ffff630a3b2bf9440b1bc8b3
silantyevmn <5992033@mail.ru>
2018-09-19 21:25:05 +0300

    add Observable

--- app/src/main/java/silantyevmn/ru/myapplication/MainPresenter.java
+++ app/src/main/java/silantyevmn/ru/myapplication/MainPresenter.java
@@ -3,27 +3,49 @@ package silantyevmn.ru.myapplication;
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 
-@InjectViewState
-public class MainPresenter extends MvpPresenter<MainView>
-{
-    CounterModel model;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
-    public MainPresenter()
-    {
+
+@InjectViewState
+public class MainPresenter extends MvpPresenter<MainView> {
+    private CounterModel model;
+
+    public MainPresenter() {
         this.model = new CounterModel();
     }
 
 
     public void counterClickButtonOne() {
-        getViewState().setButtonOneText(model.calculate(0));
+        Observable.just(0)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.computation())
+                .map(integer -> model.calculate(integer))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(integer -> getViewState().setButtonOneText(integer));
+        //model.calculateSingle(0).subscribe(integer -> getViewState().setButtonOneText(integer));
+        //getViewState().setButtonOneText(model.calculate(0));
     }
 
 
     public void counterClickButtonTwo() {
-        getViewState().setButtonTwoText(model.calculate(1));
+        Observable.just(1)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.computation())
+                .map(integer -> model.calculate(integer))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(integer -> getViewState().setButtonTwoText(integer));
+        //getViewState().setButtonTwoText(model.calculate(1));
     }
 
     public void counterClickButtonThree() {
-        getViewState().setButtonThreeText(model.calculate(2));
+        Observable.just(2)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.computation())
+                .map(integer -> model.calculate(integer))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(integer -> getViewState().setButtonThreeText(integer));
+        //getViewState().setButtonThreeText(model.calculate(2));
     }
 }

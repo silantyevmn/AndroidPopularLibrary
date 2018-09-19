a6f1f6362c91f18d9d99913aba562f0b710b359d
silantyevmn <5992033@mail.ru>
2018-09-19 21:25:18 +0300

    refactoring code

--- app/src/main/java/silantyevmn/ru/myapplication/MainView.java
+++ app/src/main/java/silantyevmn/ru/myapplication/MainView.java
@@ -5,9 +5,10 @@ import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 @StateStrategyType(AddToEndSingleStrategy.class)
-public interface MainView extends MvpView
-{
+public interface MainView extends MvpView {
     void setButtonOneText(Integer calculate);
+
     void setButtonTwoText(Integer calculate);
+
     void setButtonThreeText(Integer calculate);
 }
